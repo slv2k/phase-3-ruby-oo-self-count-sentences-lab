fb43713b4399649a5ec1cae702b6fa8bf18c585c
samuel <samuelmmanor@gmail.com>
2022-02-26 21:23:21 -0500

    commit tree 07fe4662754e4adc6ddcb243f17df77a20d19790

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,19 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    self.end_with?(".")
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-    self.end_with?("?")
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-    self.end_with?("!")
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
+    sentences = 0
+    punctuation = [".", "?", "!"]
+    self.chars.each.with_index do |char, index|
+      if punctuation.include?(char) && !punctuation.include?(self[index + 1])
+        sentences += 1
+      end
+    end
+    sentences
   end
-
 end
